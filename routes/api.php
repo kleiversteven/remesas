b83d1e0245f95c8e552f5e39e2bb567bf790df0d
kleiversteven <caraquedeveloper@gmail.com>
2018-08-22 16:14:22 -0400

    prueba api

--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::group(['prefix' => 'api'], function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('signup', 'AuthController@signup');
+
+    Route::get('login', 'AuthController@login');
   
     Route::group(['middleware' => 'auth:api'], function() {
         Route::get('logout', 'AuthController@logout');
         Route::get('user', 'AuthController@user');
     });
-});
+
